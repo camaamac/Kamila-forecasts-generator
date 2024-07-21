445171e0dc4f6608938d4ec9eaa85f4e74a1e5c7
camaamac <komilakhon1999r@gmail.com>
2024-07-21 17:28:50 +0500

    Добавила код js
    Добавила код js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-/* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const predictions = [
+    'Тебе улыбнется удача!',
+    'Получишь море радости!',
+    'Сегодня узнаешь что-то новое!',
+    'Это один из лучших дней!',
+    'Не унывай и всё получится!'
+];
 
-/* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
+function getRandomPrediction() {
+    return predictions[Math.floor(Math.random() * predictions.length)];
+}
 
-/* Подставляй текст нового предсказания в .current-forecast h1 */
+const forecastButton = document.querySelector('.forecast-btn');
+const currentForecastTitle = document.querySelector('.current-forecast h1');
+const currentForecastProbability = document.querySelector('.current-forecast p');
+const forecastsList = document.querySelector('.forecasts');
+const forecastTemplate = document.getElementById('forecast-item');
 
-/* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
+forecastButton.addEventListener('click', function() {
+    const newPrediction = getRandomPrediction();
+    const newPercentage = Math.floor(Math.random() * 101);
 
-/* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
+    currentForecastTitle.textContent = newPrediction;
+    currentForecastProbability.textContent = `Вероятность события: ${newPercentage}%`;
 
-/* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
-
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
-
-/* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+    const clone = document.importNode(forecastTemplate.content, true);
+    clone.querySelector('h3').textContent = newPrediction;
+    clone.querySelector('p').textContent = `Вероятность события: ${newPercentage}%`;
+    forecastsList.prepend(clone);
+});
